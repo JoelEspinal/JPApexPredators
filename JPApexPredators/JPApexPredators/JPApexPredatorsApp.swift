5abce653853d4437f239c220e342699cf95cdab1
JoelEspinal <joel_espinal@claro.com.do>
2024-05-22 10:11:18 -0400

    init commit with assets

---
+++ JPApexPredators/JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Joel Espinal (ClaroDom) on 22/5/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
