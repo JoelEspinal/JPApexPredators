d9e8519d195570a5b3818142344e07488b16966d
JoelEspinal <joel_espinal@claro.com.do>
2024-05-23 11:08:09 -0400

    Add type predator background label

--- JPApexPredators/JPApexPredators/ContentView.swift
+++ JPApexPredators/JPApexPredators/ContentView.swift
@@ -11,8 +11,24 @@ struct ContentView: View {
     let predators = Predators()
     var body: some View {
         List(predators.apexPredators) { predator in
-            Text(predator.name)
-        }
+            HStack {
+                Image(predator.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100.0, height: 100.0)
+                    .shadow(color: .white, radius: 1)
+                VStack(alignment: .leading) {
+                    Text(predator.name)
+                        .fontWeight(.bold)
+                    Text(predator.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 5)
+                        .background(predator.type.background)
+                }
+            }
+        }.preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }
 
