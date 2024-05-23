d371ed28dc13145d03765f164f229d93c341c7d3
JoelEspinal <joel_espinal@claro.com.do>
2024-05-23 11:36:03 -0400

    Add Navigation stack and navigation title

--- JPApexPredators/JPApexPredators/ContentView.swift
+++ JPApexPredators/JPApexPredators/ContentView.swift
@@ -10,25 +10,30 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     var body: some View {
-        List(predators.apexPredators) { predator in
-            HStack {
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100.0, height: 100.0)
-                    .shadow(color: .white, radius: 1)
-                VStack(alignment: .leading) {
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
+        NavigationStack {
+            List(predators.apexPredators) { predator in
+                HStack {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100.0, height: 100.0)
+                        .shadow(color: .white, radius: 1)
+                    VStack(alignment: .leading) {
+                        Text(predator.name)
+                            .fontWeight(.bold)
+                        Text(predator.type.rawValue.capitalized)
+                            .font(.subheadline)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding(.horizontal, 13)
+                            .padding(.vertical, 5)
+                            .background(predator.type.background)
+                            .clipShape(.capsule)
+                    }
                 }
             }
-        }.preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+            .navigationTitle("Apex Predators")
+        }
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }
 
