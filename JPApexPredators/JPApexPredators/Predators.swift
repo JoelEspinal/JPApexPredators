9dabd69142080372e34515eb1f214dc8e6846c83
JoelEspinal <joel_espinal@claro.com.do>
2024-05-27 16:07:06 -0400

    Add allApexPredators as master collection and to complete filter method

--- JPApexPredators/JPApexPredators/Predators.swift
+++ JPApexPredators/JPApexPredators/Predators.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 class Predators {
+    var allApexPredators: [ApexPredator] = []
     var apexPredators: [ApexPredator] = []
     
     init() {
@@ -20,7 +21,8 @@ class Predators {
                 let data = try Data(contentsOf: url)
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
-                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+                allApexPredators = try decoder.decode([ApexPredator].self, from: data)
+                apexPredators = allApexPredators
             } catch {
                 print("Error decoding JSON data: \(error)")
             }
@@ -48,8 +50,12 @@ class Predators {
     }
 
     func filter(by type: PredatorType) {
-        apexPredators = apexPredators.filter { predator in
-            predator.type == type
+        if type == .all {
+            apexPredators = allApexPredators
+        } else {
+            apexPredators = allApexPredators.filter { predator in
+                predator.type == type
+            }
         }
     }
 }
